1aef357435b8c42c09a739a2e8b977c77547bcc5
pharishkumar1306 <pharishkumar1306@gmail.com>
2021-05-31 20:26:37 +0530

    nav features

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import requests from './requests';
 import Row from './Row';
 import Banner from './Banner';
+import Nav from './Nav.js';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Banner />
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
